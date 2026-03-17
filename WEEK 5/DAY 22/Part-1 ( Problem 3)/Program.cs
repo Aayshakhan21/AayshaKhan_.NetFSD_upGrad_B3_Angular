eb93d4eb6b077b632fece9ec0eb112c05a7fce2f
Aaysha Khan <aayshakhan8717@gmail.com>
2026-03-17 15:53:58 +0530

    Add files via upload

---
+++ WEEK 5/DAY 22/Part-1 ( Problem 3)/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+// custom exception
+class InsufficientBalanceException : Exception
+{
+    public InsufficientBalanceException(string message) : base(message) { }
+}
+
+class BankAccount
+{
+    private double _balance;
+
+    public BankAccount(double balance)
+    {
+        _balance = balance;
+    }
+
+    public void Withdraw(double amount)
+    {
+        if (amount > _balance)
+        {
+            throw new InsufficientBalanceException("Withdrawal amount exceeds available balance");
+        }
+
+        _balance -= amount;
+        Console.WriteLine("Withdrawal successful! Remaining balance: " + _balance);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter Balance: ");
+        double bal = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter Withdraw Amount: ");
+        double amt = double.Parse(Console.ReadLine());
+
+        BankAccount account = new BankAccount(bal);
+
+        try
+        {
+            account.Withdraw(amt);
+        }
+        catch (InsufficientBalanceException ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Transaction completed.");
+        }
+    }
+}
