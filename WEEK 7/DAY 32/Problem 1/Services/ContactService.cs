2869ff6cbc28b3af5e7f1a4ccb04828c3e2337c1
Aaysha Khan <aayshakhan8717@gmail.com>
2026-04-01 17:08:31 +0530

    Add files via upload

---
+++ WEEK 7/DAY 32/Problem 1/Services/ContactService.cs
@@ -0,0 +1,24 @@
+﻿using WebApplication2.Models;
+
+namespace WebApplication2.Services
+{
+    public class ContactService : IContactService
+    {
+        private static List<ContactInfo> contacts = new List<ContactInfo>();
+
+        public void AddContact(ContactInfo contact)
+        {
+            contacts.Add(contact);
+        }
+
+        public List<ContactInfo> GetAllContacts()
+        {
+            return contacts;
+        }
+
+        public ContactInfo GetContactById(int id)
+        {
+            return contacts.FirstOrDefault(c => c.ContactId == id);
+        }
+    }
+}
---
+++ WEEK 7/DAY 32/Problem 2 (with service-repository pattern)/Services/MovieService.cs
@@ -0,0 +1,43 @@
+﻿using WebApplication2.Models;
+using WebApplication2.Repositories;
+
+namespace WebApplication2.Services
+{
+    public class MovieService : IMovieService
+    {
+        private readonly IMovieRepository _repository;
+
+        public MovieService(IMovieRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public List<Movie> GetMovies()
+        {
+            return _repository.GetAll();
+        }
+
+        public Movie GetMovie(int id)
+        {
+            return _repository.GetById(id);
+        }
+
+        public void AddMovie(Movie movie)
+        {
+            if (string.IsNullOrEmpty(movie.Title))
+                throw new Exception("Title is required");
+
+            _repository.Add(movie);
+        }
+
+        public void UpdateMovie(Movie movie)
+        {
+            _repository.Update(movie);
+        }
+
+        public void DeleteMovie(int id)
+        {
+            _repository.Delete(id);
+        }
+    }
+}
