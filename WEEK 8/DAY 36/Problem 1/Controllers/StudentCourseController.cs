7d4da68d2a34776f1787a36416e858ed3ec3a1f1
Aaysha Khan <aayshakhan8717@gmail.com>
2026-04-07 17:13:17 +0530

    Add files via upload

---
+++ WEEK 8/DAY 36/Problem 1/Controllers/StudentCourseController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication5.Repositories;
+
+namespace WebApplication5.Controllers
+{
+    public class StudentCourseController : Controller
+    {
+
+        private readonly IStudentRepository _repo;
+
+        public StudentCourseController(IStudentRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public IActionResult Students()
+        {
+            var students = _repo.GetStudentsWithCourse();
+            return View(students);
+        }
+
+        public IActionResult Courses()
+        {
+            var courses = _repo.GetCoursesWithStudents();
+            return View(courses);
+        }
+
+    }
+}
